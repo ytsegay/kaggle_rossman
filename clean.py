65343f01433fb1920622db56da41f0316b5edd30
Yohannes Tsegay <ytsegay@ebay.com>
2015-10-18 08:40:45 -0700

    Added the last part -- model evaluation. We have our dummy

---
+++ clean.py
@@ -0,0 +1,115 @@
+__author__ = 'ytsegay'
+
+import csv
+from time import strptime
+import numpy
+
+def main():
+    stores = {}
+
+    # read training file and match it to the stores file
+    with open("data/store.csv", "rb") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            stores[row[0]] = row
+
+    counter = 0
+
+    # read training file and match it to the stores file
+    with open("data/train.csv", "rb") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            joined = [entry.replace(",", "-") for entry in row + stores[row[0]][1:]]
+            if counter > 0:
+
+                compOpenDateTransformer(joined)
+                promo2DateTransformer(joined)
+                joined = oneHotEncPromoInterval(joined)
+                joined = transformDate(joined)
+                fillMissingValues(joined)
+
+                #joined += oneHotEncode(joined[10], ['a', 'b', 'c'])
+                #joined += oneHotEncode(joined[9], ['a', 'b', 'c', 'd'])
+                #joined += oneHotEncode(joined[7], ['a', 'b', 'c', '0'])
+
+                del joined[17]
+                del joined[2]
+
+                print ",".join(joined)
+            counter += 1
+
+
+def fillMissingValues(parts):
+    for i in xrange(len(parts)):
+        if parts[i].strip() == "":
+            parts[i] = "-1"
+
+# convert the year to a relative quantity, relative to min
+def compOpenDateTransformer(parts):
+    minYear = 1900
+    if parts[13].strip() != "" and parts[12].strip() != "":
+        parts[13] = str(int(parts[13])-minYear)
+    else:
+        parts[13] = "-1"
+        parts[12] = "-1"
+
+# same as compOpenDateTransformer
+def promo2DateTransformer(parts):
+    minYear = 2000
+    if parts[15].strip() != "" and parts[15].strip() != "":
+        parts[16] = str(int(parts[16])-minYear)
+        #return [parts[15], str(int(parts[16])-minYear)]
+    else:
+        parts[16] = "-1"
+        parts[15] = "-1"
+
+# one hot encode the promo interval and remove the textual representation
+def oneHotEncPromoInterval(parts):
+    numMonths = ['0','0','0','0','0','0','0','0','0','0','0','0','0']
+    if parts[17].strip() != "":
+        months = parts[17].strip().split("-")
+
+        for month in months:
+            numMonths[monthToNum(month)] = '1'
+    # remove the month column
+    #del parts[-1]
+    return parts + numMonths[1:]
+
+def oneHotEncode(value, allPossibleValues):
+    # construct a map of category to index
+    mp = {}
+    index = 0
+    for val in allPossibleValues:
+        mp[val] = index
+        index += 1
+
+    #
+    ret = ['0'] * index
+    ret[mp[value]] = '1'
+    return ret
+
+
+def transformDate(parts):
+    dateParts = parts[2].strip().split("-")
+    #del parts[2]
+    return parts + dateParts
+
+def monthToNum(date):
+
+    return{
+            'Jan' : 1,
+            'Feb' : 2,
+            'Mar' : 3,
+            'Apr' : 4,
+            'May' : 5,
+            'Jun' : 6,
+            'Jul' : 7,
+            'Aug' : 8,
+            'Sept' : 9,
+            'Oct' : 10,
+            'Nov' : 11,
+            'Dec' : 12
+    }[date]
+
+if __name__ == "__main__":
+    main()
---
+++ model.py
@@ -0,0 +1,56 @@
+import numpy
+import csv
+from sklearn.preprocessing import LabelEncoder
+from sklearn.cross_validation import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+import math
+
+
+__author__ = 'ytsegay'
+
+# compute root mean square percentage error
+# a measure of deviation from the truth in percentage
+def RMSPE(truth, predict):
+    assert len(truth) == len(predict)
+    total = 0.0
+    counter = 0
+    for i in xrange(len(truth)):
+        if truth[i] > 0:
+            diffPercentage = (truth[i] - predict[i])/truth[i]
+            total += (diffPercentage*diffPercentage)
+            counter += 1
+    print total, counter
+    return math.sqrt(total/counter)
+
+
+def runEVal():
+    temp = []
+    with open("c.csv", "rb") as f:
+            reader = csv.reader(f)
+            for row in reader:
+                temp.append(row)
+
+
+    data = numpy.array(temp)
+    data[:,6] = LabelEncoder().fit_transform(data[:,6]) #state holiday
+    data[:,8] = LabelEncoder().fit_transform(data[:,8]) #storetype
+    data[:,9] = LabelEncoder().fit_transform(data[:,9]) #assortment
+
+    # label
+    y = data[:,2]
+    # training
+    x = numpy.delete(data, 2, 1)
+
+    #thus far the data is in string format
+    y = y.astype(float)
+    x = x.astype(int)
+
+    xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size=0.33, random_state=42)
+
+    clf = RandomForestRegressor(n_estimators=150, min_samples_split=1)
+    clf.fit(xTrain, yTrain)
+    pred = clf.predict(xTest)
+
+    RMSPE(yTest, pred)
+
+
